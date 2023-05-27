c41de81ea304e379cff84494de3f9e68be385263
King404ay <127854725+King404ay@users.noreply.github.com>
2023-05-27 10:08:51 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
+  ['19099906004', 'King AY', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
